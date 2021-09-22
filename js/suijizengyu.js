f4eae448f7bbe4c4a9b206a367b823dcbee4e006
Haiyong <59255587+wanghao221@users.noreply.github.com>
2021-09-22 17:11:03 +0800

    Create suijizengyu.js

---
+++ js/suijizengyu.js
@@ -0,0 +1,6 @@
+import { quotes } from "http://haiyong.site/wp-includes/js/quotes.js";
+let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+console.log(
+  `%c✨ ${randomQuote}`,
+  "font-size:20px; background:#FFF; color:#581845;padding:10px; border: 3px solid #581845;border-radius:10px;"
+);
